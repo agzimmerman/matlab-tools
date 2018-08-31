ac83b9d0e0952bfa37861b342c1b00afb6a60241
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2018-08-31 18:25:52 +0200

    Delete read_dealii_sparse_matrix.m

--- dealii/read_dealii_sparse_matrix.m
+++
@@ -1,9 +0,0 @@
-function [ Matrix ] = read_dealii_sparse_matrix(FilePath)
-
-FileID = fopen(FilePath, 'rt');
-
-Numbers = textscan(FileID, '(%u,%u) %f \n');
-
-Matrix = sparse(double(Numbers{1}) + 1, double(Numbers{2}) + 1, Numbers{3});
-
-end
